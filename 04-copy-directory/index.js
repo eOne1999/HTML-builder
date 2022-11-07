76bb58099eb667407dd1c43fba53ff1ed9414352
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2022-11-07 20:38:31 +0300

    fix: implement copydir function for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,17 +4,20 @@ const path = require('path');
 const filesPath = path.join(__dirname, 'files');
 const copyPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(copyPath, { recursive: true }, (err) => {
-    if (err) throw err;
-    console.log('Create folder');
-});
-
-fs.readdir(filesPath, (err, files) => {
-    files.forEach(file => {
+function copyDir() {
+    fs.mkdir(copyPath, { recursive: true }, (err) => {
         if (err) throw err;
-        fs.copyFile(path.join(filesPath, file), path.join(copyPath, file), (err) => {
+        console.log('Create folder');
+    });
+
+    fs.readdir(filesPath, (err, files) => {
+        files.forEach(file => {
             if (err) throw err;
-            console.log('Copy file');
+            fs.copyFile(path.join(filesPath, file), path.join(copyPath, file), (err) => {
+                if (err) throw err;
+                console.log('Copy file');
+            });
         });
     });
-});
+}
+copyDir();
