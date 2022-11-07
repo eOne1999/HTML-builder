3d3c51e36797d7e474ffa7d505abfeca5cbe2242
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2022-11-07 18:33:55 +0300

    feat: add copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 
+const filesPath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
+
+fs.mkdir(copyPath, { recursive: true }, (err) => {
+    if (err) throw err;
+    console.log('Create folder');
+});
+
+fs.readdir(filesPath, (err, files) => {
+    files.forEach(file => {
+        if (err) throw err;
+        fs.copyFile(path.join(filesPath, file), path.join(copyPath, file), (err) => {
+            if (err) throw err;
+            console.log('Copy file');
+        });
+    });
+});
